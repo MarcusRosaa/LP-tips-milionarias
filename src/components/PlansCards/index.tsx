e8e948c2c834a0fc22abcdae37f681947ce628b4
Marcus Rosa <65923816+MarcusRosaa@users.noreply.github.com>
2023-10-24 11:53:04 -0300

    Update index.tsx

--- src/components/PlansCards/index.tsx
+++ src/components/PlansCards/index.tsx
@@ -8,18 +8,25 @@ export const PlansCards = () => {
         <Card>
           <h6>1 Previsão </h6>
           <div className="list">
-            <p>
-              <div><BiChevronRight/></div>
-              Recebe 1 bilhete com Odd 6
-            </p>
-            <p>
-              <div><BiChevronRight/></div>
-              Enviamos 1 bilhete e em caso de perdas enviamos no dia seguinte um novo bilhete de garantia
-            </p>
-            <p>
-              <div><BiChevronsRight/></div>
-              Caso a aposta de garantia perca, você recebe um acesso ao nosso grupo vip totalmente grátis!
-            </p>
+            <div className="description-wrapper">
+              <div className="icon-container"><BiChevronRight/></div>
+              <p>
+                Recebe 1 bilhete com Odd 6
+              </p>
+            </div>
+            <div className="description-wrapper">
+              <div className="icon-container"><BiChevronRight/></div>
+              <p>
+                Enviamos 1 bilhete e em caso de perdas enviamos no dia seguinte um novo bilhete de garantia
+              </p>
+            </div>
+            <div className="description-wrapper">
+              <div className="icon-container"><BiChevronsRight/></div>
+              <p>
+                Caso a aposta de garantia perca, você recebe um acesso ao nosso grupo vip totalmente grátis!
+              </p>
+            </div>
+            
           </div>
 
           <Price>R$390,00</Price>
@@ -40,26 +47,35 @@ export const PlansCards = () => {
         <Card>
           <h6>2 Previsões </h6>
           <div className="list">
-            <p>
-              <div><BiChevronRight/></div>
-              2 bilhetes com Odd 6 cada um
-            </p>
-            <p>
-              <div><BiChevronRight/></div>
-              Muito Alta probabilidade de vencer!
-            </p>
-            <p>
-              <div><BiChevronRight/></div>
-              Enviamos 2 bilhetes e em caso de não vencer, enviamos mais 2 bilhetes como garantia!
-            </p>
-            <p>
-              <div><BiChevronsRight/></div>
-              Caso a aposta de garantia perca, você recebe um acesso ao nosso grupo vip totalmente grátis!
-            </p>
+            <div className="description-wrapper">
+              <div className="icon-container"><BiChevronRight/></div>
+              <p>
+                2 bilhetes com Odd 6 cada um
+              </p>
+            </div>
+            <div className="description-wrapper">
+              <div className="icon-container"><BiChevronRight/></div>
+              <p>
+                Muito Alta probabilidade de vencer!
+              </p>
+            </div>
+            <div className="description-wrapper">
+              <div className="icon-container"><BiChevronRight/></div>
+              <p>
+                Enviamos 2 bilhetes e em caso de não vencer, enviamos mais 2 bilhetes como garantia!
+              </p>
+            </div>
+              
+            <div className="description-wrapper">
+              <div className="icon-container"><BiChevronsRight/></div>
+              <p>
+                Caso a aposta de garantia perca, você recebe um acesso ao nosso grupo vip totalmente grátis!
+              </p>
+            </div>
           </div>
 
-          <Price>R$390,00</Price>
-          <Installments>ou 12x 39,86</Installments>
+          <Price>R$295,00 - Cada</Price>
+          <Installments>Compre 2 e ganhe 190 reais de desconto</Installments>
 
           <ButtonContainer>
             <a href="https://go.perfectpay.com.br/PPU38CNBI8O">
